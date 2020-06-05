f33b2464687f0561b7b9a5b7a75a8a91b85823e4
ephemerno <ana4444@bk.ru>
2020-06-05 12:32:29 +0300

    code and report changed

--- nakonechnaya/lab5/lr5.asm
+++ nakonechnaya/lab5/lr5.asm
@@ -1,3 +1,6 @@
+CODE SEGMENT
+    ASSUME CS:CODE, DS:DATA, SS:AStack
+	
 AStack SEGMENT STACK
     dw 100h dup(?)
 AStack ENDS
@@ -7,28 +10,25 @@ DATA SEGMENT
 	NOT_LOADED_STR db 'Interruption unloaded $'
 DATA ENDS
 
-CODE SEGMENT
-    ASSUME CS:CODE, DS:DATA, SS:AStack
-	LOADED db ?
-	ID dw 0FFFh
-	KEEP_AX dw ?
-	KEEP_SS dw ?
-	KEEP_SP dw ?
-	KEEP_IP dw ?
-	KEEP_CS dw ?
-	PSP dw ?
-ROUT PROC FAR
+ROUT PROC far
 	jmp START
 	INTER_STR db 'All is working $'
 	INDEX db 0
-	STACK_INTER dw 100h dup(?)
-	END_STACK_INTER db ?
+	ID dw 0FFFh
+	KEEP_IP dw 0 
+	KEEP_CS dw 0 
+	PSP dw 0
+	KEEP_SS dw 0
+	KEEP_SP dw 0
+	KEEP_AX dw	0	
+	STACK_INTER dw 100 dup(?)
+	END_STACK_INTER db 0
 START:
-	mov KEEP_SS, SS
- 	mov KEEP_SP, SP
-  	mov KEEP_AX, AX
-	mov AX, CS
-	mov SS, AX
+	mov KEEP_SS, SS 
+	mov KEEP_SP, SP 
+	mov KEEP_AX, AX 
+	mov AX, seg STACK_INTER 
+	mov SS, AX 
 	mov SP, offset END_STACK_INTER
 	push BX
 	push CX
@@ -37,21 +37,22 @@ START:
 	push DS
 	push BP
 	push ES
-    	in AL, 60h
-    	cmp AL, 2Ah 
+    in AL, 60h
+    cmp AL, 2Ah 
 		je DO_REQ
-	call dword ptr cs:KEEP_IP;
-    	jmp ENDING;
+	pushf
+	call dword ptr CS:KEEP_IP;
+    jmp ENDING;
 DO_REQ:
-    	in AL, 61h
-	mov AH, AL    
-	or AL, 80h    
-	out 61h, AL  
-	xchg AH, AL    
-	out 61h, AL    
-	mov AL, 20h     
-	out 20h, AL  
-    	xor BX, BX
+	in AL, 61h 
+	mov AH, AL 
+	or AL, 80h 
+	out 61h, AL 
+	xchg AH, AL 
+	out 61h, AL 
+	mov AL, 20h 
+	out 20h, AL 
+	xor BX, BX
 	mov BL, INDEX 
 WRITE:
 	mov AH, 05h
@@ -61,7 +62,7 @@ WRITE:
 	mov CH, 00h
 	int 16h
 	or AL, AL 
-		jnz SKIP
+	jnz SKIP
 	inc BL
 	mov INDEX, BL
 	jmp ENDING
@@ -84,38 +85,39 @@ ENDING:
 	mov SS, AX
 	mov AX, KEEP_AX
 	mov AL, 20h
-	out 20h, AL
-	iret
+	out 20h, AL		
+	iret 	
 ROUT ENDP
 LAST_BYTE:
 
-LOAD_INTER PROC near
+LOAD_INTER PROC	near
 	push AX
-	push CX
+	push BX
+	push ES
 	push DX
 	mov AH, 35h
-	mov AL, 1Ch
+	mov AL, 09h 
 	int 21h
 	mov KEEP_IP, BX
 	mov KEEP_CS, ES
 	push DS
-	mov DX, offset ROUT
-	mov AX, seg ROUT
+	mov DX, offset ROUT 
+	mov AX, seg ROUT 	    
 	mov DS, AX
-	mov AH, 25h
-	mov AL, 1ch
+	mov AH, 25h		 
+	mov AL, 09h         	
 	int 21h
 	pop DS
 	mov DX, offset LAST_BYTE
 	mov CL, 4h
 	shr DX, CL
-	add DX, CODE
-	inc DX 
-	xor AX, AX
+	inc DX
+	add DX, 10h
 	mov AH, 31h
 	int 21h
 	pop DX
-	pop CX
+	pop ES
+	pop BX
 	pop AX
 	ret
 LOAD_INTER ENDP
@@ -124,79 +126,74 @@ UNLOAD_INTER PROC near
 	push AX
 	push BX
 	push DX
-	push DI
-	cli
-	mov AH, 35h
-	mov AL, 1Ch
-	int 21h
-	mov DI, offset KEEP_IP
-	sub DI, offset ROUT
-	mov DX, ES:[BX+DI]
-	mov AX, ES:[BX+DI+2]
-	push DS
-	mov DS, AX
-	mov AH, 25h
-	mov AL, 1Ch
-	int 21h
-	pop DS
-	mov AX, ES:[BX+DI+4]
-	mov ES, AX
 	push ES
-	mov AX, ES:[2Ch]
-	mov ES, AX
-	mov AH, 49h
-	int 21h
-	pop ES
-	mov AH, 49h
-	int 21h
+	push SI
+	cli 
+	mov	AH, 35h
+	mov	AL, 09h
+	int	21h
+	mov	SI, offset KEEP_IP
+	sub	SI, offset ROUT		
+	push DS
+	mov	DX, ES:[BX + SI]
+	mov	AX, ES:[BX + SI + 2]
+	mov	DS, AX
+	mov	AH, 25h
+	mov	AL, 09h
+	int	21h
+	pop	DS	
+	mov	AX, ES:[BX + SI + 4]
+	mov	ES, AX
+	push ES
+	mov	AX, ES:[2Ch]
+	mov	ES, AX
+	mov	AH, 49h
+	int	21h
+	pop	ES
+	mov	AH, 49h
+	int	21h
 	sti
-	pop DI
-	pop DX
-	pop BX
-	pop AX
+	pop	SI
+	pop	ES
+	pop	DX
+	pop	BX
+	pop	AX
 	ret
 UNLOAD_INTER ENDP
 
 CHECK_UNLOAD PROC near
-	mov AX, PSP
-	mov ES, AX
 	cmp byte ptr ES:[81h+1], '/'
 		jne NOT_UN
 	cmp byte ptr ES:[81h+2], 'u'
 		jne NOT_UN
 	cmp byte ptr ES:[81h+3], 'n'
 		jne NOT_UN
-	mov DX, offset NOT_LOADED_STR
-	mov AH, 09h
-    	int 21h
-	call UNLOAD_INTER
-	jmp STOP
+	mov BX, 1
 NOT_UN:
-	mov AL, LOADED 
-	cmp AL, 1
-		je STOP
-	mov DX, offset LOADED_STR
-	mov AH, 09h
-    	int 21h
-	call LOAD_INTER
-STOP:
 	ret
 CHECK_UNLOAD ENDP
 
 CHECK_LOAD PROC near
+	push BX
+	push DX
+	push SI
 	push ES
 	mov AH, 35h
-	mov AL, 1Ch
+	mov AL, 09h
 	int 21h
-	mov DX, ES:[BX-2]
+	lea SI, ID
+	sub SI, offset ROUT 
+	mov AX, 1
+	mov BX, ES:[BX + SI]
+	cmp BX, ID
+		je EXIT
+	mov AX, 0
+EXIT:
 	pop ES
-	cmp DX, ID
-		je GO
-	jmp EXIT
-	GO:
-		mov LOADED, 1
-	EXIT:
-		ret
+	pop SI
+	pop DX
+	pop BX
+	ret
 CHECK_LOAD ENDP
 
 MAIN PROC far
@@ -206,7 +203,25 @@ MAIN PROC far
 	mov PSP, AX	
 	call CHECK_LOAD
 	call CHECK_UNLOAD
-	
+	cmp BX, 1
+		je UNLOAD
+LOAD:
+	lea DX, LOADED_STR
+	push AX
+	mov AH, 09h
+	int 21h
+	pop AX
+	call LOAD_INTER
+	jmp GO_DOS
+UNLOAD:	
+	lea DX, NOT_LOADED_STR
+	push AX
+	mov AH, 09h
+	int 21h
+	pop AX
+	call UNLOAD_INTER
+	jmp GO_DOS
+GO_DOS:
 	; Выход в DOS
 	xor AL,AL
 	mov AH,4Ch
